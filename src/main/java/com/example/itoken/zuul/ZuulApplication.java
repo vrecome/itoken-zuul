6e2a99ae0e5c5afce6ecfb0fbf828f4488e119bb
vrecome <997657550@qq.com>
2018-11-29 17:14:53 +0800

    itoken commit

---
+++ src/main/java/com/example/itoken/zuul/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.example.itoken.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
